b1d6e62156fbfd1d790e2c0144e2ed41fbd03799
Derek Gerson <derekgrsn@gmail.com>
2015-09-02 10:56:21 -0700

    add default image

--- trails-log/app/helpers/application_helper.rb
+++ trails-log/app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     if user.avatar_url.present?
       user.avatar_url
     else
-      default_url = "#{root_url}images/guest.png"
+      default_url = "http://imgur.com/8CWEJHv.png"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
       "http://gravatar.com/avatar/#{gravatar_id}.png?s=100&d=#{CGI.escape(default_url)}"
     end
