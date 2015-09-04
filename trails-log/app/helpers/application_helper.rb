751b38b02f107373d70d985221be6e1117d10a68
Derek Gerson <derekgrsn@gmail.com>
2015-09-03 20:38:43 -0700

    fix form

--- trails-log/app/helpers/application_helper.rb
+++ trails-log/app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     else
       default_url = "http://imgur.com/8CWEJHv.png"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=100&d=#{CGI.escape(default_url)}"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=150&d=#{CGI.escape(default_url)}"
     end
   end
 end
